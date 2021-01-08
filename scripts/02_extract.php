09883c3fce75413fdf836333d15451b666264c4a
kiang <kiange@gmail.com>
2021-01-08 17:02:15 +0800

    skip records without Longitude value

--- scripts/02_extract.php
+++ scripts/02_extract.php
@@ -4,14 +4,17 @@ $pool = [];
 foreach(glob($basePath . '/raw/*.json') AS $jsonFile) {
     $json = json_decode(file_get_contents($jsonFile), true);
     foreach($json['features'] AS $f) {
+        if(empty($f['attributes']['Lontitude'])) {
+            continue;
+        }
         $pool[] = [
             "market_name" => $f['attributes']['market_name'],
             "addr" => $f['attributes']['addr'],
             "business_week" => $f['attributes']['business_week'],
             "context" => strip_tags($f['attributes']['context']),
             "ValidDate" => date('Y-m-d', $f['attributes']['ValidDate'] / 1000),
-            "Lontitude" => $f['attributes']['Lontitude'],
-            "Latitude" => $f['attributes']['Latitude'],
+            "Lontitude" => floatval($f['attributes']['Lontitude']),
+            "Latitude" => floatval($f['attributes']['Latitude']),
             "case_code" => $f['attributes']['case_code'],
             "last_edited_date" => date('Y-m-d H:i:s', $f['attributes']['last_edited_date'] / 1000),
             "type" => $f['attributes']['type'],
